5764a12601828721a794069e6c29a23fcc201861
GamerMan7799 <GamerMan7799@users.noreply.github.com>
2015-03-03 16:02:32 -0500

    Update to V2.1, rename to basic.h

--- basic.h
+++ basic.h
@@ -1,19 +1,37 @@
-#ifndef _CASECHANGER_H_INCLUDED__ //Guard the header so if it was already called once it isn't called again
-#define _CASECHANGER_H_INCLUDED__
+#ifndef _BASIC_H_INCLUDED__ //Guard the header so if it was already called once it isn't called again.
+#define _BASIC_H_INCLUDED__
 
 /*
 Made By: Patrick J. Rye
-Purpose: A header to hold the case changing functions, in a header as these should not be changed often and to test the options that headers give me.
-Current Revision: 1.0
+Purpose: A header to hold functions that are pretty basic and likely won't change very often or at all.
+Current Revision: 2.1
 Change Log---------------------------------------------------------------------------------------------------------------------------------------------------
 Date	Revision	Changed By			Changes
 ------  ---------   ------------		---------------------------------------------------------------------------------------------------------------------
 =============================================================================================================================================================	
 2/20/15	1.0			Patrick Rye			-Original from 3.1a
-=============================================================================================================================================================		
+=============================================================================================================================================================
+3/2/15	2.0			Patrick Rye			-Renamed to Basic.h
+										-Added save file checker function.
+=============================================================================================================================================================
+3/2/15	2.1			Patrick Rye			-Changed save file checker to be able to check for any file name.
+=============================================================================================================================================================
 */
 
 using namespace std;
+
+
+bool fileexists(const char *fileName)
+{
+    std::ifstream infile(fileName);
+    return infile.good();
+}
+
+bool saveexists()
+{
+  ifstream ifile("save.bif");
+  return ifile;
+}
 
 std::string ConvertToUpper(std::string& str)
 {
