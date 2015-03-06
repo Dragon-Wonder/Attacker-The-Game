22fde045edc1b4b6301fc30a25879090253b7ad7
GamerMan7799 <GamerMan7799@users.noreply.github.com>
2015-03-06 12:54:07 -0500

    Update to V2.1.2
    2015/03/04	2.1.1		Patrick Rye		-Removed save exists function as it was replaced by file exists.
    
    2015/03/05	2.1.2 		Patrick Rye		-Changed change log date format from MM/DD/YY to YYYY/MM/DD because I like it better.

--- basic.h
+++ basic.h
@@ -4,33 +4,30 @@
 /*
 Made By: Patrick J. Rye
 Purpose: A header to hold functions that are pretty basic and likely won't change very often or at all.
-Current Revision: 2.1
+Current Revision: 2.1.2
 Change Log---------------------------------------------------------------------------------------------------------------------------------------------------
-Date	Revision	Changed By			Changes
-------  ---------   ------------		---------------------------------------------------------------------------------------------------------------------
+Date		Revision	Changed By		Changes
+------  	---------   ------------	---------------------------------------------------------------------------------------------------------------------
 =============================================================================================================================================================	
-2/20/15	1.0			Patrick Rye			-Original from 3.1a
+2015/02/20	1.0			Patrick Rye		-Original from 3.1a
 =============================================================================================================================================================
-3/2/15	2.0			Patrick Rye			-Renamed to Basic.h
+2015/03/02	2.0			Patrick Rye		-Renamed to Basic.h
 										-Added save file checker function.
 =============================================================================================================================================================
-3/2/15	2.1			Patrick Rye			-Changed save file checker to be able to check for any file name.
+2015/03/02	2.1			Patrick Rye		-Changed save file checker to be able to check for any file name.
 =============================================================================================================================================================
+2015/03/04	2.1.1		Patrick Rye		-Removed save exists function as it was replaced by file exists.
+=============================================================================================================================================================
+2015/03/05	2.1.2 		Patrick Rye		-Changed change log date format from MM/DD/YY to YYYY/MM/DD because I like it better.
+=============================================================================================================================================================	
 */
 
 using namespace std;
-
 
 bool fileexists(const char *fileName)
 {
     std::ifstream infile(fileName);
     return infile.good();
-}
-
-bool saveexists()
-{
-  ifstream ifile("save.bif");
-  return ifile;
 }
 
 std::string ConvertToUpper(std::string& str)
@@ -67,6 +64,5 @@ char CharConvertToUpper(char chrCheck)
 	converted = (toupper(chrCheck, settings));
 	return converted;
 }
-
 
 #endif //If header was already called load nothing
