ae05f4c2371b030aad1288a97a19ec8b5e4ab9b0
GamerMan7799 <GamerMan7799@users.noreply.github.com>
2015-03-17 13:45:39 -0400

    Update main to V1.0c
    2015/03/13	1.0c		Patrick Rye		-Move from beta revisions to gamma revisions.
    										-Changed some int to smaller variables because they don't need to be that big.
    										-Fixed bug where new dungeon wasn't generating properly. (This is what happens when you don't play test changes for a while).
    										-Made it so you have to be in debug mode to force a monster (Don't know why you would want this otherwise)
    										-General code improvement.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
 Made By: Patrick J. Rye
 Purpose: A game I made as an attempt to teach myself c++, just super basic, but going to try to keep improving it as my knowledge increases.
-Current Revision: 1.0c-dev3
+Current Revision: 1.0c
 Change Log---------------------------------------------------------------------------------------------------------------------------------------------------
 Date		Revision	Changed By		Changes
 ------  	---------   ------------	---------------------------------------------------------------------------------------------------------------------
@@ -13,6 +13,8 @@ Date		Revision	Changed By		Changes
 2015/03/13	1.0c		Patrick Rye		-Move from beta revisions to gamma revisions.
 										-Changed some int to smaller variables because they don't need to be that big.
 										-Fixed bug where new dungeon wasn't generating properly. (This is what happens when you don't play test changes for a while).
+										-Made it so you have to be in debug mode to force a monster (Don't know why you would want this otherwise)
+										-General code improvement.
 =============================================================================================================================================================	
 */
 
@@ -30,27 +32,27 @@ Date		Revision	Changed By		Changes
 #include "basic.h" //Functions that are simple and won't need to be changed very often.
 #include "battle.h" //Functions that deal with battling, levelling up and making a player.
 #include "rooms.h" //Functions that deal with generating a dungeon.
-#include "spells.h" //Functions that deal with spells and magic, a possible future addition.
+#include "spells.h" //Functions that deal with spells and magic
 /*********************************************************************************************************/
 using namespace std;
 Dungeon d; //Define the dungeon class as 'd' so I can use functions in there anywhere later in the code.
 /*********************************************************************************************************/
 //Make all the global variables that I need.
-short intMainLevel = 1; //The level of the dungeon.
-short intLevelStart = 1; //The level that the game starts at. Will be 1 unless loading from a save.
+unsigned char intMainLevel = 1; //The level of the dungeon.
+unsigned char intLevelStart = 1; //The level that the game starts at. Will be 1 unless loading from a save.
 bool blDebugMode = false; //If game is in debug mode or not, effects if player has access to debug commands.
-const string CurrentVerison = "1.0c-dev3"; //The current version of this program, stored in a save file later on.
+const string CurrentVerison = "1.0c"; //The current version of this program, stored in a save file later on.
 /*********************************************************************************************************/
 //These functions have to be up here as functions in save.h use them.
 //These values are used to pass values to the save header so that they may be saved.
 //Or to set values from a load.
-short getmainvalue(unsigned short intvalue)
+int getmainvalue(unsigned char intvalue)
 {
 	if(intvalue == 0 ) {return intMainLevel;}
 	else if (intvalue == 1) {return intLevelStart;}
 	else {return 1;}
 }
-void setmainvalue(unsigned short intlocation, unsigned short intvalue) {if (intlocation == 0) {intLevelStart = intvalue;}}
+void setmainvalue(unsigned char intlocation, unsigned int intvalue) {if (intlocation == 0) {intLevelStart = intvalue;}}
 void setdebugmode(bool blsetdebugmode) {blDebugMode = blsetdebugmode;}
 #include "save.h" //A header to hold functions related to saving and loading.
 /*********************************************************************************************************/
@@ -87,7 +89,7 @@ int main()
 	if(!blOldSave) //If it is not an old save show welcome message.
 	{
 		cout<<endl<<"Your objective is to go through 10 randomly generated dungeons."<<endl;
-		cout<<"You are looking for the stairs down ( > ). While you are represented by † ."<<endl;
+		cout<<"You are looking for the stairs down ( > ). While you are represented by @ ."<<endl;
 		cout<<"Every step brings you closer to your goal, but there might be a monster there as well."<<endl;
 		cout<<"Each level is harder than the last, do you have what it takes to win?"<<endl;
 		cout<<"Good luck!"<<endl<<endl<<endl<<endl;
@@ -107,7 +109,7 @@ int main()
 		{
 			cout << string(50, '\n');
 			d.showDungeon();
-			cout<<"Level "<<intMainLevel<<" of 10."<<endl;
+			cout<<"Level "<<(int)intMainLevel<<" of 10."<<endl;
 			cout<<"Please enter a direction you would like to go: "<<endl;
 			cout<<"[N]orth,  [E]ast,  [S]outh,  [W]est  "<<endl;
 			cout<<"E[X]it,   [C]heck your health, [H]eal,   Sa[V]e  "<<endl;
@@ -134,7 +136,7 @@ int main()
 			}
 			if (!(charExitFind=='S') && !(charPlayerDirection == 'C')) //If player did not save or did not check himself, see if player runs into monster.
 			{
-				if(rand() % 101 <= 10 || charExitFind == 'M') //Random chance to encounter monster, or debug code to force monster encounter.
+				if(rand() % 101 <= 10 || (charExitFind == 'M' && blDebugMode)) //Random chance to encounter monster, or debug code to force monster encounter.
 				{
 					cout << string(50, '\n');
 					blBattleEnding = startbattle(intMainLevel); //Starts battle.
